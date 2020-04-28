280a7590d4c8f8cf850442d5d1f5963cccc68413
SprytnyMarek <skrzypkowski.pawel@wp.pl>
2020-04-28 13:32:51 +0200

    hi

--- HelloDumitrus/src/fds.java
+++ HelloDumitrus/src/fds.java
@@ -2,7 +2,7 @@ public class fds
 {
   public static void main(String[] args)
   {
-    System.out.println("Dumitrus Boguś");
+    System.out.println("Dumitrus Boguś134");
 
     System.out.println("DUmitrus Pawelus");
   }
