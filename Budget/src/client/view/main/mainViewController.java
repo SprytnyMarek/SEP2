ed52affa517fa4a0ebcbf30177352fe4422b3690
Wojciech <womielczarek@gmail.com>
2020-05-15 14:51:05 +0200

    I made couple of changes in mainView.fxml and mainViewController

--- Budget/src/client/view/main/mainViewController.java
+++ Budget/src/client/view/main/mainViewController.java
@@ -2,9 +2,28 @@ package client.view.main;
 
 import client.core.ViewHandler;
 import client.view.login.LoginVM;
+import com.gluonhq.charm.glisten.control.ProgressIndicator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class mainViewController
 {
+  @FXML
+  private Label budgetLabel;
+  @FXML
+  private Label newBudgetLabel;
+  @FXML
+  private TextField budgetField;
+  @FXML
+  private Button setButton;
+  @FXML
+  private Label amountLabel;
+  @FXML
+  private ProgressIndicator progressIndicator;
+
   private ViewHandler vh;
   private MainVM vm;
 
@@ -14,5 +33,9 @@ public class mainViewController
   {
     this.vm = mainVm;
     this.vh = viewHandler;
+  }
+
+  public void onSetButton(ActionEvent actionEvent)
+  {
   }
 }
